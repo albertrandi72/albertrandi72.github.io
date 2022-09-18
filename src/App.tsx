ba94087c4f4841f9a76a0488c4e70b3241274288
albert randi <albertrandi72@gmail.com>
2022-09-18 23:02:40 +0700

    use card design in biography

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,29 @@
 import React from "react";
-import logo from "./logo.svg";
 import "./App.css";
-
+import { GoPerson } from "react-icons/go";
 function App() {
   return (
-    <div className='App'>
-      <div className='text-4xl font-semibold my-10'>
+    <div className='App bg-gray-100'>
+      <div className='text-4xl font-semibold my-10 mt-0 pt-4'>
         <div className='pb-2'>Albert Randi</div>
         <div className='pb-2'>Web Dev</div>
         <div className='pb-2'>Based in Indonesia</div>
       </div>
-      <div className='py-10 px-4'>
-        <div className='pb-8'>
-          <div className='text-gray-500 font-bold'>Biography</div>
-          <div className='text-xl my-5 font-semibold'>
-            Just a ordinary coder, deep thinker and a strategic game player
+      <div className='flex justify-center'>
+        <div className='rounded shadow w-48 bg-white'>
+          <div className='flex justify-center'>
+            <GoPerson size={140} />
+          </div>
+
+          <div className=' border-t-2'>
+            <div className='text-gray-500 font-bold'>Biography</div>
+            <div className='text-sm m-2 font-semibold '>
+              Just a ordinary coder, deep thinker and a strategic game player
+            </div>
           </div>
         </div>
+      </div>
+      <div className='py-10 px-4'>
         <div className='pb-8'>
           <div className='text-gray-500 font-bold'>Contact</div>
           <div className='text-xl my-5 font-semibold'>
